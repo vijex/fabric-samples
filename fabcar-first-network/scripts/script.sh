9c275f0b3f2a8c8c2038c5474723f2c8df2dd0a1
vijex <40830522+vijex@users.noreply.github.com>
2018-08-26 16:52:06 +0530

    Error fix
     query chaincode() error check removed

--- fabcar-first-network/scripts/script.sh
+++ fabcar-first-network/scripts/script.sh
@@ -11,6 +11,441 @@ echo "Build your first network (BYFN) end-to-end test"
 echo
 CHANNEL_NAME="$1"
 DELAY="$2"
+1
+
+#!/bin/bash
+
+2
+
+​
+
+3
+
+echo
+
+4
+
+echo " ____    _____      _      ____    _____ "
+
+5
+
+echo "/ ___|  |_   _|    / \    |  _ \  |_   _|"
+
+6
+
+echo "\___ \    | |     / _ \   | |_) |   | |  "
+
+7
+
+echo " ___) |   | |    / ___ \  |  _ <    | |  "
+
+8
+
+echo "|____/    |_|   /_/   \_\ |_| \_\   |_|  "
+
+9
+
+echo
+
+10
+
+echo "Build your first network (BYFN) end-to-end test"
+
+11
+
+echo
+
+12
+
+CHANNEL_NAME="$1"
+
+13
+
+DELAY="$2"
+
+14
+
+LANGUAGE="$3"
+
+15
+
+TIMEOUT="$4"
+
+16
+
+VERBOSE="$5"
+
+17
+
+: ${CHANNEL_NAME:="mychannel"}
+
+18
+
+: ${DELAY:="3"}
+
+19
+
+: ${LANGUAGE:="golang"}
+
+20
+
+: ${TIMEOUT:="10"}
+
+21
+
+: ${VERBOSE:="false"}
+
+22
+
+LANGUAGE=`echo "$LANGUAGE" | tr [:upper:] [:lower:]`
+
+23
+
+COUNTER=1
+
+24
+
+MAX_RETRY=5
+
+25
+
+​
+
+26
+
+CC_SRC_PATH="github.com/chaincode/fabcar/go/"
+
+27
+
+if [ "$LANGUAGE" = "node" ]; then
+
+28
+
+        CC_SRC_PATH="/opt/gopath/src/github.com/chaincode/fabcar/node/"
+
+
+97
+
+​
+
+98
+
+22
+
+99
+
+​
+
+100
+
+LANGUAGE=`echo "$LANGUAGE" | tr [:upper:] [:lower:]`
+
+101
+
+​
+
+102
+
+23
+
+103
+
+​
+
+104
+
+COUNTER=1
+
+105
+
+​
+
+106
+
+24
+
+107
+
+​
+
+108
+
+MAX_RETRY=5
+
+109
+
+​
+
+110
+
+25
+
+111
+
+​
+
+112
+
+•
+
+113
+
+​
+
+114
+
+26
+
+115
+
+​
+
+116
+
+CC_SRC_PATH="github.com/chaincode/fabcar/go/"
+
+117
+
+​
+
+118
+
+27
+
+119
+
+​
+
+120
+
+if [ "$LANGUAGE" = "node" ]; then
+
+121
+
+​
+
+122
+
+28
+
+123
+
+​
+
+124
+
+        CC_SRC_PATH="/opt/gopath/src/github.com/chaincode/fabcar/node/"
+
+125
+
+​
+
+126
+
+29
+
+127
+
+​
+
+128
+
+fi
+
+129
+
+​
+
+130
+
+30
+
+131
+
+​
+
+132
+
+•
+
+133
+
+​
+
+134
+
+31
+
+135
+
+​
+
+136
+
+echo "Channel name : "$CHANNEL_NAME
+
+137
+
+​
+
+138
+
+32
+
+139
+
+​
+
+140
+
+•
+
+141
+
+​
+
+142
+
+33
+
+143
+
+​
+
+144
+
+# import utils
+
+145
+
+​
+
+146
+
+34
+
+147
+
+​
+
+148
+
+. scripts/utils.sh
+
+149
+
+​
+
+150
+
+35
+
+151
+
+​
+
+152
+
+•
+
+153
+
+​
+
+154
+
+36
+
+155
+
+​
+
+156
+
+createChannel() {
+29
+
+fi
+
+30
+
+​
+
+31
+
+echo "Channel name : "$CHANNEL_NAME
+
+32
+
+​
+
+33
+
+# import utils
+
+34
+
+. scripts/utils.sh
+
+35
+
+​
+
+36
+
+createChannel() {
+
+37
+
+        setGlobals 0 1
+
+38
+
+​
+
+39
+
+        if [ -z "$CORE_PEER_TLS_ENABLED" -o "$CORE_PEER_TLS_ENABLED" = "false" ]; then
+
+40
+
+                set -x
+
+41
+
+                peer channel create -o orderer.example.com:7050 -c $CHANNEL_NAME -f ./channel-artifacts/channel.tx >&log.txt
+
+42
+
+                res=$?
+
+43
+
+                set +x
+
+44
+
+        else
+
+45
+
+                                set -x
+
+46
+
+                peer channel create -o orderer.example.com:7050 -c $CHANNEL_NAME -f ./channel-artifacts/channel.tx --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA >&log.txt
+
+47
+
+                res=$?
+
+48
+
+                                set +x
+
+49
+
+        fi
 LANGUAGE="$3"
 TIMEOUT="$4"
 VERBOSE="$5"
@@ -90,7 +525,7 @@ instantiateChaincode 0 2
 
 # Query chaincode on peer0.org1
 echo "Querying chaincode on peer0.org1..."
-chaincodeQuery 0 1 100
+chaincodeQuery 0 1 
 
 # Invoke chaincode on peer0.org1 and peer0.org2
 echo "Sending invoke transaction on peer0.org1 peer0.org2..."
@@ -102,9 +537,9 @@ installChaincode 1 2
 
 # Query on chaincode on peer1.org2, check if the result is 90
 echo "Querying chaincode on peer1.org2..."
-chaincodeQuery 1 2 90
+chaincodeQuery 1 2 
 
-echo
+
 echo "========= All GOOD, BYFN execution completed =========== "
 echo
 
